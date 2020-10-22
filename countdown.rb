4fd5e0dd73d4cba5f0a3da70e61feeec5cbe3e82
Yormynnelly Grajales <yormynnelly.grajales@gmail.com>
2020-10-22 12:20:32 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown(timer)
+
+    while timer > 0
+        puts "#{timer} SECOND(S)!"
+        timer -= 1
+    end
+
+    "HAPPY NEW YEAR!"
+
+end
+
+
+def countdown_with_sleep(timer)
+
+    while timer > 0
+        puts "#{timer} SECOND(S)!"
+        sleep(1)
+        timer -= 1
+    end
+
+    "HAPPY NEW YEAR!"
+
 end
